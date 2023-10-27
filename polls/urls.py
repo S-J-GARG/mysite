be44e1000f5963852c1d1cb63a90b9be034b4251
S-J-GARG <shivankgarg321kax@gmail.com>
2023-10-27 14:14:50 +0530

    ok

--- polls/urls.py
+++ polls/urls.py
@@ -1,5 +1,20 @@
 from django.urls import path
 from . import views
+
+#! app_name="polls" is used for name spacing
+app_name="polls"
 urlpatterns=[
-    path("",views.index,name="index"),
+    
+    # #url for index## /polls/index
+    # path("",views.index,name="index"),
+    # #urls for details of question***/polls/2/
+    # path("<int:question_id>/",views.details,name="details"),
+    # path("<int:question_id>/results/",views.results,name="results"),
+    # path("<int:question_id>/vote/",views.vote,name="vote"),
+    #TODO For writing Genric views 
+    path("", views.IndexView.as_view(), name="index"),
+    path("<int:pk>/", views.DetailView.as_view(), name="details"),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
+    path("<int:question_id>/vote/", views.vote, name="vote"),
+
 ]
