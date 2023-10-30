6485989c3de5ddbfb9d57958511e665f3a76d8ee
S-J-GARG <shivankgarg321kax@gmail.com>
2023-10-30 17:29:57 +0530

    completed my Django first course

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
 from .models import Question,Choise
 # Register your models here.
-admin.site.register(Question)
-admin.site.register(Choise)
+
+class ChoiceInline(admin.TabularInline):
+    '''One small problem, though. It takes a lot of screen space to display all the fields for entering related Choice objects. For that reason, Django offers a tabular way of displaying inline related objects. To use it, change the ChoiceInline declaration to read:'''
+    model = Choise
+    extra = 3
+class QuestionAdmin(admin.ModelAdmin):
+     fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"]}),
+    ]
+     inlines = [ChoiceInline]
+     '''By default, Django displays the str() of each object. But sometimes it’d be more helpful if we could display individual fields. To do that, use the list_display admin option, which is a tuple of field names to display, as columns, on the change list page for the object:
+     '''
+     list_display = ["question_text", "pub_date"]
+     list_filter = ["pub_date"]
+
+
+admin.site.register(Question,QuestionAdmin)
+'''This is insufient to call choices bcoz it list out all choices in admin page but not load in question wise'''
+# admin.site.register(Choise)
--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,7 @@
 import datetime
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 # Create your models here.
 
 # Model for Questions set######################
@@ -8,11 +9,15 @@ from django.utils import timezone
 class Question(models.Model):
     question_text=models.CharField(max_length=200)
     pub_date=models.DateTimeField("date published")
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",)
     def __str__(self):
         return self.question_text
     def was_published_recently(self):
-        return self.pub_date >=timezone.now()-datetime.timedelta(days=1)
-
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 #Model for choises made by user ###########
 class Choise(models.Model):
